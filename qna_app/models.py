4279d3e668bae0b6520c0ddc28f43ba89a8e02a5
amishachaudhary <50077735+amishachaudhary@users.noreply.github.com>
2019-12-11 08:56:22 +0545

    models changed

--- qna_app/models.py
+++ qna_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+#models creates database automatically (ORM(OBJECT RELATION MODEL) CONCEPT)
+
+class QuestionModel(models.Model):
+    #id is made by django automatically (built-in)
+    title = models.CharField(max_length=255)
+    posted_by = models.CharField(max_length=120)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    question_desc = models.TextField()
+    qn_img = models.ImageField(upload_to='QuestionImg')
+
+
+class AnswerModel(models.Model):
+    answered_by = models.CharField(max_length=120)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    answer_desc = models.TextField()
+    answer_img = models.ImageField(upload_to='AnswerImg')
+    question_id = models.ForeignKey(QuestionModel)
+    votes = models.IntegerField()
+    is_accept = models.BooleanField()
+    question = models.ForeignKey(QuestionModel,on_delete=models.CASCADE)
+
+
